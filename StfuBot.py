6b9ec1f3f36d119c2a2a62990c43283daede55ce
Leon Simmon <33871545+rylox@users.noreply.github.com>
2019-09-23 16:16:08 +0200

    added auto disconnect

--- StfuBot.py
+++ StfuBot.py
@@ -1,5 +1,6 @@
 import re
 import discord
+import asyncio
 
 
 def is_valid_ipv4(data):
@@ -26,17 +27,38 @@ class Bot(discord.Client):
 
         if is_valid_ipv4(message.content):
             msg = '{0.author.mention} stfu'.format(message)
-            self.currently_tracked = message.author
+
+            if self.currently_tracked is None:
+                self.currently_tracked = message.author
+
             await message.channel.send(msg)
             await message.delete()
 
             voice_state = message.author.voice
+
             if voice_state is not None:
-                self.voice_client = await voice_state.channel.connect()
+                if self.voice_client is not None:
+                    if not self.voice_client.is_connected():
+                        self.currently_tracked = message.author
+                        self.voice_client = await voice_state.channel.connect()
+                    else:
+                        if self.voice_client.is_playing():
+                            return
+                        else:
+                            self.currently_tracked = message.author
+                            await self.voice_client.move_to(voice_state.channel)
+                else:
+                    self.voice_client = await voice_state.channel.connect()
+
                 await self.play_file()
 
     async def on_voice_state_update(self, member, before, after):
         if member == self.user:
+
+            if after.channel is None:
+                if self.voice_client is not None:
+                    if self.voice_client.is_playing():
+                        self.voice_client.stop()
             return
 
         if member == self.currently_tracked:
@@ -53,8 +75,23 @@ class Bot(discord.Client):
 
     async def play_file(self):
         if self.voice_client is not None:
-            source = await discord.FFmpegOpusAudio.from_probe("stfu.mp3")
-            self.voice_client.play(source)
+            if self.voice_client.is_connected():
+                if not self.voice_client.is_playing():
+                    source = await discord.FFmpegOpusAudio.from_probe("stfu.mp3")
+                    self.voice_client.play(source, after=self.finished_playing)
+
+    def finished_playing(self, error):
+        if self.voice_client.is_playing():
+            self.voice_client.stop()
+
+        self.currently_tracked = None
+        coro = self.voice_client.disconnect()
+        fut = asyncio.run_coroutine_threadsafe(coro, self.loop)
+        try:
+            fut.result()
+        except:
+            # an error happened sending the message
+            pass
 
 
 def main():
